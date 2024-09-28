7199e750f02a4863ffbe1fa44dd3aae78b2e2e87
shravanis1009 <shravani.shinde109@gmail.com>
2024-09-29 00:25:28 +0530

    initial commit

---
+++ frontend/src/routes/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      heheher
+    </div>
+  )
+}
+
+export default About
---
+++ frontend/src/routes/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      chinguu
+    </div>
+  )
+}
+
+export default Home
