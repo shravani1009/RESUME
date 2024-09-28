5473c2771b1e960a33286d9a4dcc8ab2e828e20e
shravanis1009 <shravani.shinde109@gmail.com>
2024-09-29 03:04:45 +0530

    initial commit

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,16 +1,22 @@
 import "./NavbarStyles.css"
 import { Link } from "react-router-dom"
-import React from 'react'
-import { FaBars } from "react-icons/fa"
+import React, { useState } from 'react'
+import { FaBars, FaTimes } from "react-icons/fa"
 
 
 const Navbar = () => {
+
+    const[click,setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+
+
+
   return (
     <div className="header">
       <Link to="/">
         <h1>PORTFOLIO</h1>
       </Link>
-      <ul className="nav-menu">
+      <ul className={click ? "nav-menu.active" : "nav-menu"}>
         <li>
             <Link to= "/">Home</Link>
         </li>
@@ -24,8 +30,10 @@ const Navbar = () => {
             <Link to= "/contact">Contact</Link>
         </li>
       </ul>
-      <div className="humburger">
-        <FaBars size={20} style={{color: "#fff"}} />
+      <div className="humburger" onClick={handleClick}>
+        {click ? ( <FaTimes size={20} style={{color: "#fff"}} />):(  <FaBars size={20} style={{color: "#fff"}} />)}
+       
+      
       </div>
     </div>
   )
