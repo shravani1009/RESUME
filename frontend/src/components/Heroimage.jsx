84d44e6d72abdc19fb39956aaff868bd7a4c0931
shravanis1009 <shravani.shinde109@gmail.com>
2024-09-30 01:07:34 +0530

    initial commit

--- frontend/src/components/Heroimage.jsx
+++ frontend/src/components/Heroimage.jsx
@@ -1,15 +1,30 @@
 import "./Heroimagestyles.css";
 
 import React from 'react'
+import girlcoding from '../assets/girlcoding.jpg'
+import Girll from '../assets/Girll.jpg'
+import girl from '../assets/girl.jpg'
+import code from '../assets/code.jpg'
+import { Link } from "react-router-dom";
 
 const Heroimage = () => {
   return (
     <div className="hero">
         <div className="mask">
-            
-            </div>    
+          <img className="intro-img"
+          src = {code} alt="girl"/>  
+        </div>
+        <div className="content">
+          <p>Hi There! My name is Shravani Shinde and I am a</p>
+          <h1>Web developer , Programmer & Graphic Designer</h1>
+          
+          
+         
+          <Link to = "/project" className="btn">Projects</Link>
+          <Link to = "/contact" className="btn btn-light ">CONTACT</Link>
+          </div>    
     </div>
   )
 }
 
-export default Heroimage
+export default Heroimage;
