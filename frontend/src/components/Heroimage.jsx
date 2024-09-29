37ad077f6a1fb33db23b96bc4ae3a134346a0fe0
shravanis1009 <shravani.shinde109@gmail.com>
2024-09-29 15:55:21 +0530

    initial commit

---
+++ frontend/src/components/Heroimage.jsx
@@ -0,0 +1,15 @@
+import "./Heroimagestyles.css";
+
+import React from 'react'
+
+const Heroimage = () => {
+  return (
+    <div className="hero">
+        <div className="mask">
+            
+            </div>    
+    </div>
+  )
+}
+
+export default Heroimage
