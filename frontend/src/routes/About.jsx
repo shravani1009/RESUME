6ab554be5f786c0136b982914c882524a10d3e51
shravanis1009 <shravani.shinde109@gmail.com>
2024-10-06 17:20:11 +0530

    aboutcontent

--- frontend/src/routes/About.jsx
+++ frontend/src/routes/About.jsx
@@ -3,11 +3,13 @@ import Navbar from '../components/navbar'
 import Footer from '../components/Footer'
 import HEROimg from '../components/HEROimg'
 
+
 const About = () => {
   return (
     <div>
        <Navbar/>
        <HEROimg heading = "ABOUT" />
+       
        <Footer/>
 
     </div>
--- frontend/src/routes/Contact.jsx
+++ frontend/src/routes/Contact.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import Navbar from '../components/navbar'
 import Footer from '../components/Footer'
 import HEROimg from '../components/HEROimg'
-
+import AboutContent from '../components/AboutContent'
 const Contact = () => {
   return (
     <div>
        <Navbar/>
        <HEROimg heading = "CONTACT" text = "Lets have a chat"/>
+       <AboutContent/>
        <Footer/>
       
     </div>
