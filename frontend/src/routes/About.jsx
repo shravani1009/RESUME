d08675dcb1444af9ef3904a957c0690b7e472338
shravanis1009 <shravani.shinde109@gmail.com>
2024-10-07 12:38:03 +0530

    initial commit

--- frontend/src/routes/About.jsx
+++ frontend/src/routes/About.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import Navbar from '../components/navbar'
 import Footer from '../components/Footer'
 import HEROimg from '../components/HEROimg'
-
+import AboutContent from '../components/AboutContent'
 
 const About = () => {
   return (
     <div>
        <Navbar/>
        <HEROimg heading = "ABOUT" />
-       
+       <AboutContent/>       
        <Footer/>
 
     </div>
