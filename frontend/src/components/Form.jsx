5b41a9fcbcd9ba1c2f14f8122ac918d9558c59dc
shravanis1009 <shravani.shinde109@gmail.com>
2024-10-07 13:57:00 +0530

    initial commit

---
+++ frontend/src/components/Form.jsx
@@ -0,0 +1,22 @@
+import "./FormsStyles.css"
+import React from 'react'
+
+const Form = () => {
+  return (
+    <div className="form">
+        <form>
+            <label>Your Name</label>
+            <input type="text"></input>
+            <label>Email</label>
+            <input type="email"></input>
+            <label>Message</label>
+            <textarea rows="6" placeholder="Type your message here"></textarea>
+            <button className="btn"> Submit </button>
+
+        </form>
+      
+    </div>
+  )
+}
+
+export default Form
--- frontend/src/routes/Contact.jsx
+++ frontend/src/routes/Contact.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import Navbar from '../components/navbar'
 import Footer from '../components/Footer'
 import HEROimg from '../components/HEROimg'
+import Form from '../components/Form'
 
 const Contact = () => {
   return (
     <div>
        <Navbar/>
        <HEROimg heading = "CONTACT" text = "Lets have a chat"/>
-       
+       <Form/>
        <Footer/>
       
     </div>
