ad6add43fd0809eda22a41c36d271013b1bc73a0
shravanis1009 <shravani.shinde109@gmail.com>
2024-10-07 13:05:34 +0530

    initial commit

--- frontend/src/components/AboutContent.jsx
+++ frontend/src/components/AboutContent.jsx
@@ -1,34 +1,34 @@
-import "./AboutContentStyles.css"
-import coffee from "../assets/coffee.jpg"
-import girl from "../assets/girl.jpg"
+import "./AboutContentStyles.css";
+import coffee from "../assets/coffee.jpg";
+import girl from "../assets/girl.jpg";
 
-
-import React from 'react'
+import React from "react";
 import { Link } from "react-router-dom";
+
 const AboutContent = () => {
   return (
     <div className="about">
-        <div className="left">
-            <h1></h1>
-            <p>Let's build something amazing together! Feel free to reach out if you'd like to discuss a project, collaboration, or just want to say hello. I'm always excited to connect with new people</p>
-            <Link to="/contact">
-                <button className="btn">Contact</button>          
-            </Link>   
+      <div className="left">
+        <h1>Hey Folks!</h1>
+        <p>
+          Let's build something amazing together! Feel free to reach out if you'd
+          like to discuss a project, collaboration, or just want to say hello.
+          I'm always excited to connect with new people.
+        </p>
+        <Link to="/contact">
+          <button className="btn">Contact</button>
+        </Link>
+      </div>
+      <div className="right">
+        <div className="img-container">
+          <div className="img-stack top">
+            <img src={coffee} className="img" alt="coffee" />
+          </div>
+         
         </div>
-        <div className="right">
-            <div className="img-container">
-                <div className="img-stack top">
-                    <img src={coffee} className="img" alt="true"></img>
-                </div>
-                <div className="img-stack bottom">
-                    <img src={girl} className="img" alt="true"></img>
-                </div>
-            </div>
-
-        </div>
-
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AboutContent
+export default AboutContent;
