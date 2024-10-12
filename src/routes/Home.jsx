fb55fac7ddbb2673b560916de52b611889ceb0b5
shravanis1009 <shravani.shinde109@gmail.com>
2024-10-12 23:12:00 +0530

    initial

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from '../components/Navbar.jsx';
+import Navbar from '../components/Navbar';
 import Heroimage from '../components/Heroimage'
 import Footer from '../components/Footer'
 import Experience from '../components/experience'
