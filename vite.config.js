40151a1019d9d795f327bd3ee098ee4debdc4442
shravanis1009 <shravani.shinde109@gmail.com>
2024-10-13 01:38:32 +0530

    initial

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/', // or adjust to a subpath if deploying under one
+  plugins: [react()],
   build: {
     outDir: 'dist',
-    emptyOutDir: true
+    assetsDir: 'assets',
   }
-});
+})
